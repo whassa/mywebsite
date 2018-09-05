b62282b5dd047f42f315143bc065409b9fa340d8
remi Graton <remi.graton@gmail.com>
2018-09-05 13:47:22 -0400

    Change to php and add error404

---
+++ error404.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8" /> 
+        <title> Rémi Graton </title>
+        <link rel="stylesheet" href="includes/bootstrap-3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/home.css">
+    </head>
+    <body>
+    
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                 </button>
+              <a class="navbar-brand" href="#">Rémi Graton</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="https://github.com/whassa" target="_blank"> Liens vers mon github. <span class="sr-only">(current)</span></a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <div class="content"> 
+            <div id="textInfo" class="col-md-6 col-md-offset-3 col-lg-4  col-lg-offset-4"> 
+                <h2>Perdu ?</h2>
+                <p>Vous explorez des facettes de ma vie qui ne sont pas disponible pour vous.  Vous pouvez revenir à la page d'acceuil avec le lien suivant : <a href="">ici</a></p>
+            </div>
+        </div>
+        
+    </body>
+    
+    <script src="includes/jquery-3.2.0.min.js"></script>
+    <script src="includes/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+</html> 
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8" /> 
+        <title> Rémi Graton </title>
+        <link rel="stylesheet" href="includes/bootstrap-3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/home.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                 </button>
+              <a class="navbar-brand" href="#">Rémi Graton</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="https://github.com/whassa" target="_blank"> Liens vers mon github. <span class="sr-only">(current)</span></a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <div class="content"> 
+            <div id="textInfo" class="col-md-6 col-md-offset-3 col-lg-4  col-lg-offset-4"> 
+                <h2>Rémi Graton</h2><br />
+                <p>
+                    Bonjour, mon nom est Rémi Graton. Je suis présentement étudiant en génie logiciel à l'ÉTS.
+                    Après avoir fini ma technique en informatique à l'école montmorency en 2017, j'ai décidé de pousser mes connaissances dans une université comme l'ÉTS.
+                    Avec mes stages chez <a href="http://orangetango.com/fr">OrangeTango</a> 
+                    et chez <a href="https://oneprovider.com/">OneProvider</a>,
+                    j'ai acquis beaucoup d'expérience dans la création de site internet, ainsi que la gestion de certaine plateforme de gestion.
+                    J'aimerais également découvrir le domaine du jeu vidéo.
+                    Je pense être quelqu'un d'apte à entreprendre n'importe quelle sorte de projet et je suis capable de m'adapter à toute sorte de problème
+                    qui me fait face. Dans le menu du haut, vous pouvez regardez mon github si vous désirez voir mes petits projets.
+                </p>
+            </div>
+        </div>
+        
+    </body>
+    
+    <script src="includes/jquery-3.2.0.min.js"></script>
+    <script src="includes/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+</html> 
